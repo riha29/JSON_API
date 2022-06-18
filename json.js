91cf8f767590066634cf1185e4a4c23cf0271d87
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2022-06-18 23:46:56 +0600

    Add files via upload

---
+++ json.js
@@ -0,0 +1,9 @@
+const shop= {
+    name: 'alia',
+    address: 'ranbir road'
+}
+console.log(shop)
+const shop1= JSON.stringify(shop)
+console.log(shop1)
+const shop2= JSON.parse(shop1)
+console.log(shop2)
