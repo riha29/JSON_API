323bfa01e022fb208f99a1228c521be59ac60cd5
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2022-06-25 10:20:26 +0600

    completed finding people

--- buddy.js
+++ buddy.js
@@ -8,12 +8,13 @@ loadBuddies()
 
 const dataOfBuddies= data => {
     const buddies= data.results
-    console.log(data.results)
-    const buddiesDiv= document.getElementById('buddies')
+    // console.log(data.results)
+    const buddiesDiv= document.getElementById('buddy')
     for( const buddy of buddies){
-        console.log(buddy.email)
+        // console.log(buddy.email)
+        console.log(buddy.name.first, buddy.name.last, buddy.email)
         const p= document.createElement('p')
-        p.innerText= buddy.email
+        p.innerText= `Name:${buddy.name.first} ${buddy.name.last} Email:${buddy.email}`
         buddiesDiv.appendChild(p)
     }
 }
